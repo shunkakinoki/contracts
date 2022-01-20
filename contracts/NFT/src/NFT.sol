8a61a32717fb2eb18a75755731cb06f27998a69a
Shun Kakinoki <shunkakinoki@gmail.com>
2022-01-19 23:41:32 -0800

    chore: update

--- contracts/NFT/src/NFT.sol
+++ contracts/NFT/src/NFT.sol
@@ -4,28 +4,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-contract NFT is ERC721URIStorage, Ownable {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-
-  constructor() public ERC721("Nft", "NFT") {}
-
-  function mintNFT(address recipient, string memory tokenURI)
-    public
-    onlyOwner
-    returns (uint256)
-  {
-    _tokenIds.increment();
-
-    uint256 newItemId = _tokenIds.current();
-    _mint(recipient, newItemId);
-    _setTokenURI(newItemId, tokenURI);
-
-    return newItemId;
-  }
-}
+contract TWiDAOs is ERC721Upgradeable {}
---
+++ contracts/TWiDAOs/src/TWiDAOs.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract KiyoSanBirthday is ERC721Enumerable, ReentrancyGuard, Ownable {
+  using Counters for Counters.Counter;
+
+  constructor(string memory customBaseURI_)
+    ERC721("KiyoSanBirthday", "0xKiyo")
+  {
+    customBaseURI = customBaseURI_;
+  }
+
+  uint256 public constant MAX_SUPPLY = 27;
+
+  mapping(address => uint256) private mintCountMap;
+
+  mapping(address => uint256) private allowedMintCountMap;
+
+  uint256 public constant MINT_LIMIT_PER_WALLET = 1;
+
+  function allowedMintCount(address minter) public view returns (uint256) {
+    return MINT_LIMIT_PER_WALLET - mintCountMap[minter];
+  }
+
+  function updateMintCount(address minter, uint256 count) private {
+    mintCountMap[minter] += count;
+  }
+
+  function mint() public nonReentrant {
+    if (allowedMintCount(_msgSender()) >= 1) {
+      updateMintCount(_msgSender(), 1);
+    } else {
+      revert("Minting limit exceeded");
+    }
+
+    require(totalSupply() < MAX_SUPPLY, "Exceeds max supply");
+
+    _safeMint(_msgSender(), totalSupply());
+  }
+
+  string private customBaseURI;
+
+  function setBaseURI(string memory customBaseURI_) external onlyOwner {
+    customBaseURI = customBaseURI_;
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return customBaseURI;
+  }
+}
