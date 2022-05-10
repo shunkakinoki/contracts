e4219a876595ff9425d8ff62ec9ad1ffc0c63cc1
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-10 11:22:37 -0700

    feat: ini merkle airdrop starter

---
+++ contracts/MerkleAirdropStarter/src/tests/MerkleAirdropStarter.t.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: Unlicense
+// Code from: https://raw.githubusercontent.com/Anish-Agnihotri/merkle-airdrop-starter/master/contracts/src/test/MerkleAirdropStarter.t.sol
+
+pragma solidity ^0.8.13;
+
+/// ============ Imports ============
+
+import { MerkleAirdropStarterTest } from "./utils/MerkleAirdropStarterTest.sol"; // Test scaffolding
+
+/// @title Tests
+/// @notice MerkleAirdropStarter tests
+/// @author Anish Agnihotri <contact@anishagnihotri.com>
+contract Tests is MerkleAirdropStarterTest {
+  /// @notice Allow Alice to claim 100e18 tokens
+  function testAliceClaim() public {
+    // Setup correct proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Collect Alice balance of tokens before claim
+    uint256 alicePreBalance = ALICE.tokenBalance();
+
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      100e18,
+      // With valid proof
+      aliceProof
+    );
+
+    // Collect Alice balance of tokens after claim
+    uint256 alicePostBalance = ALICE.tokenBalance();
+
+    // Assert Alice balance before + 100 tokens = after balance
+    assertEq(alicePostBalance, alicePreBalance + 100e18);
+  }
+
+  /// @notice Prevent Alice from claiming twice
+  function testFailAliceClaimTwice() public {
+    // Setup correct proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      100e18,
+      // With valid proof
+      aliceProof
+    );
+
+    // Claim tokens again
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      100e18,
+      // With valid proof
+      aliceProof
+    );
+  }
+
+  /// @notice Prevent Alice from claiming with invalid proof
+  function testFailAliceClaimInvalidProof() public {
+    // Setup incorrect proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xc11ae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      100e18,
+      // With valid proof
+      aliceProof
+    );
+  }
+
+  /// @notice Prevent Alice from claiming with invalid amount
+  function testFailAliceClaimInvalidAmount() public {
+    // Setup correct proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // Incorrect: 1000 tokens
+      1000e18,
+      // With valid proof (for 100 tokens)
+      aliceProof
+    );
+  }
+
+  /// @notice Prevent Bob from claiming
+  function testFailBobClaim() public {
+    // Setup correct proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Claim tokens
+    BOB.claim(
+      // Claiming for Bob
+      address(BOB),
+      // 100 tokens
+      100e18,
+      // With valid proof (for Alice)
+      aliceProof
+    );
+  }
+
+  /// @notice Let Bob claim on behalf of Alice
+  function testBobClaimForAlice() public {
+    // Setup correct proof for Alice
+    bytes32[] memory aliceProof = new bytes32[](1);
+    aliceProof[
+      0
+    ] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+
+    // Collect Alice balance of tokens before claim
+    uint256 alicePreBalance = ALICE.tokenBalance();
+
+    // Claim tokens
+    BOB.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      100e18,
+      // With valid proof (for Alice)
+      aliceProof
+    );
+
+    // Collect Alice balance of tokens after claim
+    uint256 alicePostBalance = ALICE.tokenBalance();
+
+    // Assert Alice balance before + 100 tokens = after balance
+    assertEq(alicePostBalance, alicePreBalance + 100e18);
+  }
+}
