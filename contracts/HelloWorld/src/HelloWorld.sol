a876406fcb78ef0f3c4d995b9f6ce9f8fa59858f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-10 23:01:36 -0700

    feat: ini hello world

---
+++ contracts/HelloWorld/src/HelloWorld.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract HelloWorld {
+  uint256 public greet = 300;
+}
---
+++ contracts/HelloWorld/src/tests/HelloWorld.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../HelloWorld.sol";
+
+contract HelloWorldTest is Test {
+  uint256 testNumber;
+  HelloWorld private helloWorld;
+
+  function setUp() public {
+    helloWorld = new HelloWorld();
+  }
+
+  function testNumberIs42() public {
+    bytes32 leet = vm.load(address(helloWorld), bytes32(uint256(0)));
+    emit log_uint(uint256(leet));
+    assertEq(uint256(leet), 300);
+  }
+
+  function testFailSubtract43() public {
+    testNumber -= 43;
+  }
+}
