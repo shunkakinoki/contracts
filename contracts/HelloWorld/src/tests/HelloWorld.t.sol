f7713b0c40cb58ea3f868575cd1af06642d4172e
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-10 23:03:50 -0700

    chore: update

--- contracts/HelloWorld/src/tests/HelloWorld.t.sol
+++ contracts/HelloWorld/src/tests/HelloWorld.t.sol
@@ -6,20 +6,15 @@ import "forge-std/Test.sol";
 import "../HelloWorld.sol";
 
 contract HelloWorldTest is Test {
-  uint256 testNumber;
   HelloWorld private helloWorld;
 
   function setUp() public {
     helloWorld = new HelloWorld();
   }
 
-  function testNumberIs42() public {
+  function testNumberIs300() public {
     bytes32 leet = vm.load(address(helloWorld), bytes32(uint256(0)));
     emit log_uint(uint256(leet));
     assertEq(uint256(leet), 300);
-  }
-
-  function testFailSubtract43() public {
-    testNumber -= 43;
   }
 }
