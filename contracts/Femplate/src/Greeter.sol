ec5650f602cc19480c274a863333c101e463e844
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-12 14:11:17 -0700

    feat: ini Femplate

---
+++ contracts/Femplate/src/Greeter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Code from: https://raw.githubusercontent.com/abigger87/femplate/master/src/Greeter.sol
+
+pragma solidity 0.8.13;
+
+/// @title Greeter
+/// @author andreas@nascent.xyz
+contract Greeter {
+  string public _gm;
+  address public owner;
+
+  // CUSTOMS
+  error BadGm();
+  event GMEverybodyGM();
+
+  constructor(string memory newGm) {
+    _gm = newGm;
+    owner = msg.sender;
+  }
+
+  function gm(string memory myGm) external returns (string memory greeting) {
+    if (
+      keccak256(abi.encodePacked((myGm))) !=
+      keccak256(abi.encodePacked((greeting = _gm)))
+    ) revert BadGm();
+    emit GMEverybodyGM();
+  }
+
+  function setGm(string memory newGm) external {
+    _gm = newGm;
+  }
+}
