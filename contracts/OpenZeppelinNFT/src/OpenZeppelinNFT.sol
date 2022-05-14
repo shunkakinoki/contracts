c664ad86c01a0f8518d4f3b9cab2ad9d2b82140c
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-13 21:55:52 -0700

    feat: ini openzepplin nft

---
+++ contracts/OpenZeppelinNFT/src/OpenZeppelinNFT.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OpenZeppelinNft is ERC721, PullPayment, Ownable {
+  using Strings for uint256;
+  string public baseURI;
+  uint256 public currentTokenId;
+  uint256 public constant TOTAL_SUPPLY = 10_000;
+  uint256 public constant MINT_PRICE = 0.08 ether;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _baseURI
+  ) ERC721(_name, _symbol) {
+    baseURI = _baseURI;
+  }
+
+  function mintTo(address recipient) public payable returns (uint256) {
+    require(
+      msg.value == MINT_PRICE,
+      "Transaction value did not equal the mint price"
+    );
+    uint256 newTokenId = ++currentTokenId;
+    require(newTokenId <= TOTAL_SUPPLY, "Max supply reached");
+    _safeMint(recipient, newTokenId);
+    return newTokenId;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      ownerOf(tokenId) != address(0),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    return
+      bytes(baseURI).length > 0
+        ? string(abi.encodePacked(baseURI, tokenId.toString()))
+        : "";
+  }
+
+  /// @dev Overridden in order to make it an onlyOwner function
+  function withdrawPayments(address payable payee) public override onlyOwner {
+    super.withdrawPayments(payee);
+  }
+}
