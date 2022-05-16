bb0160d1b541b111d96e7e2c99ddd94e671948c3
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-15 19:31:58 -0700

    perf: ini v2 claim

--- contracts/WagumiCatsV2/src/WagumiCatsV2.sol
+++ contracts/WagumiCatsV2/src/WagumiCatsV2.sol
@@ -3,12 +3,22 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WagumiCatsV2 is ERC721, Ownable {
   uint256 public constant totalSupply = 3000;
   uint256 public constant mintPrice = 0.03 ether;
+  address public constant wagumiCatsNFT =
+    0x6144D927EE371de7e7f8221b596F3432E7A8e6D9;
+
   uint256 public currentTokenId;
+
+  event Claimed(uint256 indexed tokenId, address indexed claimer);
+  error NotOwner();
+  error AlreadyRedeemed();
+
+  mapping(uint256 => bool) public hasClaimed;
 
   constructor() ERC721("Wagumi Cats v2", "WAGUMI") {}
 
@@ -18,5 +28,19 @@ contract WagumiCatsV2 is ERC721, Ownable {
       uint256 newTokenId = ++currentTokenId;
       _safeMint(msg.sender, newTokenId);
     }
+  }
+
+  function claim(uint256 tokenId) external payable {
+    if (hasClaimed[tokenId]) {
+      revert AlreadyRedeemed();
+    }
+    if (IERC721(wagumiCatsNFT).ownerOf(tokenId) != msg.sender) {
+      revert NotOwner();
+    }
+
+    hasClaimed[tokenId] = true;
+    emit Claimed(tokenId, msg.sender);
+    uint256 newTokenId = ++currentTokenId;
+    _safeMint(msg.sender, newTokenId);
   }
 }
--- contracts/WagumiCatsV2/src/tests/WagumiCatsV2.t.sol
+++ contracts/WagumiCatsV2/src/tests/WagumiCatsV2.t.sol
@@ -3,17 +3,31 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../WagumiCatsV2.sol";
 
+contract WagumiCats is ERC721("Wagumi Cats", "WAGUMI") {
+  uint256 public tokenId = 1;
+
+  function mint() public returns (uint256) {
+    _mint(msg.sender, tokenId);
+    return tokenId++;
+  }
+}
+
 contract WagumiCatsV2Test is Test {
+  WagumiCats private og;
   WagumiCatsV2 private nft;
+  uint256 internal nftId;
   address receiver = address(1);
 
   function setUp() public {
+    og = new WagumiCats();
     nft = new WagumiCatsV2();
+    nftId = og.mint();
   }
 
-  function testPreMintForTreasuryMultiSig() public {
+  function testPreMintForTreasury() public {
     assertEq(nft.balanceOf(address(receiver)), 0);
 
     nft.transferOwnership(address(receiver));
