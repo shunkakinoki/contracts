4211dca2d2c5fadf2f89922f28f61a0922d5e67f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-21 20:35:12 -0700

    chore: ini token testing

--- contracts/WagumiToken/src/WagumiToken.sol
+++ contracts/WagumiToken/src/WagumiToken.sol
@@ -2,6 +2,16 @@
 
 pragma solidity ^0.8.13;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WagumiToken {}
+contract WagumiToken is ERC20, ERC20Burnable, Ownable {
+  constructor() ERC20("Wagumi DAO", "WAGUMI") {
+    _mint(_msgSender(), 10_000_000 * 1e18);
+  }
+
+  function mint(address _to, uint256 _amount) external onlyOwner {
+    _mint(_to, _amount);
+  }
+}
--- contracts/WagumiToken/src/tests/WagumiToken.t.sol
+++ contracts/WagumiToken/src/tests/WagumiToken.t.sol
@@ -3,19 +3,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import { WagumiToken } from "../WagumiToken.sol";
 
 contract WagumiTokenTest is Test {
   uint256 testNumber;
+  WagumiToken token;
+  address deployer = address(1);
 
   function setUp() public {
-    testNumber = 42;
+    vm.prank(deployer);
+    token = new WagumiToken();
   }
 
-  function testNumberIs42() public {
-    assertEq(testNumber, 42);
+  function testOwner() public {
+    vm.prank(deployer);
+    assertEq(token.owner(), deployer);
   }
 
-  function testFailSubtract43() public {
-    testNumber -= 43;
+  function testOwnerCanMint() public {
+    vm.prank(deployer);
+    token.mint(address(2), 3);
+  }
+
+  function testOwnerCannnotMint() public {
+    vm.prank(address(3));
+    vm.expectRevert("Ownable: caller is not the owner");
+    token.mint(address(2), 3);
+    vm.stopPrank();
   }
 }
