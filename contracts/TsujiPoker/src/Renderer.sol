683732b5d87f8f5435f17cb14ef98bed16573ea9
Shun Kakinoki <shunkakinoki@gmail.com>
2022-05-29 17:22:18 -0700

    chore: update

--- contracts/TsujiPoker/src/Renderer.sol
+++ contracts/TsujiPoker/src/Renderer.sol
@@ -19,7 +19,7 @@ contract Renderer is ENSNameResolver {
 
     return
       string.concat(
-        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">Tsuji Poker</text><text x="10" y="40" class="base">Player:</text><text x="10" y="60" class="base">',
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">Tsuji Poker Night</text><text x="10" y="40" class="base">Player:</text><text x="10" y="60" class="base">',
         ownerString,
         '</text><text x="10" y="80" class="base">Rank:</text><text x="10" y="100" class="base">',
         rankString,
