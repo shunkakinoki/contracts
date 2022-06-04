55943d2108f1958f4462cba1d33c2477756d99d8
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-04 09:46:29 -0700

    chore: update

--- contracts/EVM/src/EVM.sol
+++ contracts/EVM/src/EVM.sol
@@ -3,21 +3,45 @@
 pragma solidity ^0.8.13;
 
 contract EVM {
+  // 01: ADD
   function add(uint256 a, uint256 b) public pure returns (uint256 result) {
     assembly {
       result := add(a, b)
     }
   }
 
+  // 02: MUL
   function mul(uint256 a, uint256 b) public pure returns (uint256 result) {
     assembly {
       result := mul(a, b)
     }
   }
 
+  // 03: SUB
   function sub(uint256 a, uint256 b) public pure returns (uint256 result) {
     assembly {
       result := sub(a, b)
+    }
+  }
+
+  // 04: DIV
+  function div(uint256 a, uint256 b) public pure returns (uint256 result) {
+    assembly {
+      result := div(a, b)
+    }
+  }
+
+  // 05: SDIV
+  function sdiv(uint256 a, uint256 b) public pure returns (uint256 result) {
+    assembly {
+      result := sdiv(a, b)
+    }
+  }
+
+  // 06: MOD
+  function mod(uint256 a, uint256 b) public pure returns (uint256 result) {
+    assembly {
+      result := mod(a, b)
     }
   }
 }
--- contracts/EVM/src/tests/EVM.t.sol
+++ contracts/EVM/src/tests/EVM.t.sol
@@ -12,7 +12,7 @@ contract EVMTest is Test {
     evm = new EVM();
   }
 
-  // GAS 3: add(uint256, uint256)
+  // 01 GAS 3: add(uint256, uint256)
   function testAdd() public {
     assertEq(evm.add(1, 2), 3);
     // overflows on 2**256
@@ -27,7 +27,7 @@ contract EVMTest is Test {
     );
   }
 
-  // GAS 5: mul(uint256, uint256)
+  // 02 GAS 5: mul(uint256, uint256)
   function testMul() public {
     assertEq(evm.mul(1, 2), 2);
     // overflows on 2**256
@@ -42,7 +42,7 @@ contract EVMTest is Test {
     );
   }
 
-  // GAS 3: sub(uint256, uint256)
+  // 03 GAS 3: sub(uint256, uint256)
   function testSub() public {
     assertEq(evm.sub(10, 10), 0);
     // underflow on 2**256
@@ -52,5 +52,46 @@ contract EVMTest is Test {
       evm.sub(0, 1),
       0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
     );
+  }
+
+  // 04 GAS 3: div(uint256, uint256)
+  function testDiv() public {
+    assertEq(evm.div(10, 10), 1);
+    // underflow on 2**256 - if the denominator is 0, resuit=0.
+    assertEq(evm.div(1, 2), 0);
+    // underflow on 2**256
+    assertEq(evm.div((2**256 - 2), (2**256 - 1)), 0);
+    // same as above
+    assertEq(
+      evm.div(
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE,
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+      ),
+      0
+    );
+  }
+
+  // 05 GAS 5: sdiv(uint256, uint256)
+  function testSdiv() public {
+    assertEq(evm.sdiv(10, 10), 1);
+    // underflow on 2**256 - if the denominator is 0, resuit=0.
+    assertEq(evm.sdiv(1, 2), 0);
+    // overflows on -2**255
+    assertEq(evm.sdiv((2**256 - 2), (2**256 - 1)), 2);
+    // same as above
+    assertEq(
+      evm.sdiv(
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE,
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+      ),
+      2
+    );
+  }
+
+  // 06 GAS 5: mod(uint256, uint256)
+  function testMod() public {
+    assertEq(evm.mod(10, 3), 1);
+    // underflow on 2**256
+    assertEq(evm.mod(17, 5), 2);
   }
 }
