cac14ee2609f0d418083df80c2e3bfe67e3fa561
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-04 14:02:50 -0700

    chore: update

--- contracts/EIP712/src/test/EIP712.t.sol
+++ contracts/EIP712/src/test/EIP712.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
-import { Test } from "forge-std/test.sol";
+import { Test } from "forge-std/Test.sol";
 
 import { EIP712 } from "../EIP712.sol";
 
