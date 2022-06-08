394b6801238d6bee5074b2d8fbb84fbdd29e51a7
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-08 13:38:49 -0700

    chore: update

--- contracts/TheMerge/src/TheMerge.sol
+++ contracts/TheMerge/src/TheMerge.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.13;
 
+// Code from: https://twitter.com/m1guelpf/status/1529340774286073857?s=20&t=I_X_6QPRQzU9xZpdo-6dqA
 contract TheMerge {
   function hasMergeSucceeded() public view returns (bool) {
     return block.difficulty > 2**64;
