49aa581b0cf06fceb2fa31b6ed6b0c605125ad69
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-10 20:49:16 -0700

    perf: ini staking ohm

---
+++ contracts/OlympusDAO/src/interfaces/IBondingCalculator.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity ^0.8.13;
+
+interface IBondingCalculator {
+  function markdown(address _LP) external view returns (uint256);
+
+  function valuation(address pair_, uint256 amount_)
+    external
+    view
+    returns (uint256 _value);
+}
