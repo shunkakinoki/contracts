fa4035d5e9d9e4d2e33156f3d1c2e9d0093052e9
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-21 22:59:15 -0700

    perf: ini huff deployer

--- contracts/HuffDeployer/src/HuffDeployer.sol
+++ contracts/HuffDeployer/src/HuffDeployer.sol
@@ -1,20 +1,48 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.13;
-import "forge-std/Test.sol";
 
-contract HuffDeployer is Test {
-  function deployContract() public returns (string memory) {
-    string[] memory inputs = new string[](3);
-    inputs[0] = "echo";
-    inputs[1] = "-n";
-    inputs[
-      2
-    ] = "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002676d000000000000000000000000000000000000000000000000000000000000";
+///@notice This cheat codes interface is named _CheatCodes so you can use the CheatCodes interface in other testing files without errors
+interface _CheatCodes {
+  function ffi(string[] calldata) external returns (bytes memory);
+}
 
-    bytes memory res = vm.ffi(inputs);
-    string memory output = abi.decode(res, (string));
+contract HuffDeployer {
+  address constant HEVM_ADDRESS =
+    address(bytes20(uint160(uint256(keccak256("hevm cheat code")))));
 
-    return output;
+  /// @notice Initializes cheat codes in order to use ffi to compile Huff contracts
+  _CheatCodes cheatCodes = _CheatCodes(HEVM_ADDRESS);
+
+  ///@notice Compiles a Huff contract and returns the address that the contract was deployeod to
+  ///@notice If deployment fails, an error will be thrown
+  ///@param fileName - The file name of the Huff contract. For example, the file name for "SimpleStore.huff" is "SimpleStore"
+  ///@return deployedAddress - The address that the contract was deployed to
+
+  function deploy(string memory fileName) public returns (address) {
+    ///@notice create a list of strings with the commands necessary to compile Huff contracts
+    string[] memory cmds = new string[](4);
+    cmds[0] = "./node_modules/.bin/huffc";
+    cmds[1] = string(string.concat(fileName, ".huff"));
+    cmds[2] = "--bytecode";
+    cmds[3] = "-n";
+
+    ///@notice compile the Huff contract and return the bytecode
+    bytes memory bytecode = cheatCodes.ffi(cmds);
+
+    ///@notice deploy the bytecode with the create instruction
+    address deployedAddress;
+    assembly {
+      deployedAddress := create(0, add(bytecode, 0x20), mload(bytecode))
+    }
+
+    ///@notice check that the deployment was successful
+    require(
+      deployedAddress != address(0),
+      "HuffDeployer could not deploy contract"
+    );
+
+    ///@notice return the address that the contract was deployed to
+    return deployedAddress;
   }
 }
--- contracts/HuffDeployer/src/tests/HuffDeployer.t.sol
+++ contracts/HuffDeployer/src/tests/HuffDeployer.t.sol
@@ -3,19 +3,24 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../HuffDeployer.sol";
+
+interface Number {
+  function setNumber(uint256) external;
+
+  function getNumber() external returns (uint256);
+}
 
 contract HuffDeployerTest is Test {
-  uint256 testNumber;
+  Number number;
+  HuffDeployer huffDeployer = new HuffDeployer();
 
   function setUp() public {
-    testNumber = 42;
+    number = Number(huffDeployer.deploy("huff_contracts/Number"));
   }
 
-  function testNumberIs42() public {
-    assertEq(testNumber, 42);
-  }
-
-  function testFailSubtract43() public {
-    testNumber -= 43;
+  function testNumber() public {
+    number.setNumber(1);
+    assertEq(1, number.getNumber());
   }
 }
