e9842bfae212f2775e1046963a3fa5d0fa2c83c4
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-19 09:15:12 -0700

    chore: update

---
+++ contracts/HuffHelloWorld/src/tests/HuffHelloWorld.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract HuffHelloWorldTest is Test {
+  uint256 testNumber;
+
+  function setUp() public {
+    testNumber = 42;
+  }
+
+  function testNumberIs42() public {
+    assertEq(testNumber, 42);
+  }
+
+  function testFailSubtract43() public {
+    testNumber -= 43;
+  }
+}
